105c2047f799bd30a805ee9b3f7caae94e5f16fc
Muzik4mm <christopher.barlow@ssanpete.org>
2016-07-26 13:15:43 -0600

    Used the sandwich class

--- TeacherProject/main.cpp
+++ TeacherProject/main.cpp
@@ -8,26 +8,37 @@
 
 #include <iostream>
 #include <string>
+#include "Sandwich.h"
 
 using namespace std;
 
 int main()
 {
+    Sandwich mySandwich;
+    cout << mySandwich.getName() << endl;
+    cout << "Lets rename my sandwich" << endl;
+    cout << "Type in a new name"<< endl;
+    string tempName;
+    cin >> tempName;
+    mySandwich.setName(tempName);
+    cout << "The New Sandwich name is " << mySandwich.getName() << endl;
+    
     // insert code here...
-    cout << "Hello, World!" << endl;
-    double myFraction;
-    cout << myFraction <<endl;
-    myFraction = 3.15;
-    cout << myFraction <<endl;
-    int myFavoriteNumber;
-    cout << myFavoriteNumber << endl;
-    cin >> myFavoriteNumber;
-    cout << "Your Favorite Number is ";
-    cout << myFavoriteNumber << endl;
-    bool myBoolean = true;
-    cout << myBoolean << endl;
-    string myWords;
-    myWords = "This is a test for my String";
-    cout << myWords << endl;
+//    cout << "Hello, World!" << endl;
+//    double myFraction;
+//    cout << myFraction <<endl;
+//    myFraction = 3.15;
+//    cout << myFraction <<endl;
+//    int myFavoriteNumber;
+//    cout << myFavoriteNumber << endl;
+//    cin >> myFavoriteNumber;
+//    cout << "Your Favorite Number is ";
+//    cout << myFavoriteNumber << endl;
+//    bool myBoolean = true;
+//    cout << myBoolean << endl;
+//    string myWords;
+//    myWords = "This is a test for my String";
+//    cout << myWords << endl;
+    
     return 0;
 }
